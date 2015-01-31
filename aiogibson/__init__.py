0f9ae0e1bb1813de093657597544536757f95bec
Nickolai Novik <is.infinity@yahoo.com>
2015-02-01 00:28:40 +0200

    bump version and upd changelog for release

--- aiogibson/__init__.py
+++ aiogibson/__init__.py
@@ -3,7 +3,7 @@ from .errors import (GibsonError, ProtocolError, ReplyError,
                      ExpectedANumber, MemoryLimitError, KeyLockedError)
 from .pool import GibsonPool, create_pool, create_gibson
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 # make pyflakes happy
 (GibsonConnection, create_connection, GibsonError, ProtocolError, ReplyError,
